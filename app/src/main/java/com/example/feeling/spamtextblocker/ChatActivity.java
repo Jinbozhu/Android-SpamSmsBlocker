288cbb05c6f874ccc795c28c01adaffaf513f69a
Jinbo Zhu <zjbfeeling@gmail.com>
2016-03-15 00:28:27 -0700

    copy text done

--- app/src/main/java/com/example/feeling/spamtextblocker/ChatActivity.java
+++ app/src/main/java/com/example/feeling/spamtextblocker/ChatActivity.java
@@ -1,14 +1,18 @@
 package com.example.feeling.spamtextblocker;
 
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.text.ClipboardManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -22,6 +26,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.feeling.spamtextblocker.adapters.ChatAdapter;
@@ -71,9 +76,11 @@ public class ChatActivity extends AppCompatActivity {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
+
             @Override
             public void afterTextChanged(Editable s) {
                 checkMessageLength();
@@ -121,8 +128,32 @@ public class ChatActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Reference:
+     * http://stackoverflow.com/questions/33717238/copy-list-item-in-android
+     *
+     * @param index
+     */
     private void copy(int index) {
-
+        Message msg = chatArrayList.get(index);
+        String content = msg.getContent();
+        if (content.length() > 0) {
+            try {
+                if (android.os.Build.VERSION.SDK_INT < 11) {
+                    android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setText(content);
+                } else {
+                    android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    android.content.ClipData clip = android.content.ClipData.newPlainText("WordKeeper", content);
+                    clipboard.setPrimaryClip(clip);
+                }
+                Toast.makeText(getApplicationContext(), "Text copied to clipboard.", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Error copying text to clipboard.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Empty selection.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
